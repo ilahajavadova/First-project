a7d9a3e160b3ec6f97ed0217763356c0a7ce6468
ilahajavadova <163039213+ilahajavadova@users.noreply.github.com>
2024-04-03 18:02:42 +0400

    Add files via upload

---
+++ 03.04JS/app.js
@@ -0,0 +1,45 @@
+"use strict"
+
+
+
+// let arr = [ 23, 17, 6, 45, 67, 34 ];
+// let minIndex = 0;
+
+// let min  = arr[0]
+//  for (let i = 0; i < arr.length; i++) {
+//    if (arr[i]<min) {
+//     min = arr[i]
+//     minIndex = i;
+//    }
+
+//  }
+//  console.log(min , minIndex)
+
+
+
+// let arr = [23, 17, 6, 45, 67, 34];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     for (let j = 2; j < arr[i]; j++) {
+
+//         if (arr[i] % j == 0) {
+
+//             sum += arr[i];
+
+//             break;
+//         }
+//     }
+// }
+
+// console.log(sum);
+
+let name = "Ilahe"
+let reverseName = " ";
+
+ for (  let i  =  name.length -1;  i >= 0 ; i--) {
+    reverseName += name[i]
+    
+ }
+ console.log(reverseName);
